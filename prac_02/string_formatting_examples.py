13315417d7a7c796bb200e4d8d81738bb106b125
Jackson Finnigan <31056058+JacksonFinnigan@users.noreply.github.com>
2017-08-30 11:33:25 +1000

    finished
    finished

--- prac_02/string_formatting_examples.py
+++ prac_02/string_formatting_examples.py
@@ -1,9 +1,3 @@
-# """
-# CP1404/CP5632 - Practical
-# Various examples of using Python string formatting with the str.format() method
-# Want to read more about it? https://docs.python.org/3/library/string.html#formatstrings
-# """
-#
 # name = "Gibson L-5 CES"
 # year = 1922
 # cost = 16035.40
