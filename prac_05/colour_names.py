e13c608024c2f93c0f62f2e3e5a11258541bfb6d
Jackson Finnigan <31056058+JacksonFinnigan@users.noreply.github.com>
2017-08-30 21:13:28 +1000

    Name change
    COLOURS

--- prac_05/colour_names.py
+++ prac_05/colour_names.py
@@ -1,4 +1,4 @@
-COLOUR = {"AliceBlue": "#f0f8ff", "AntiqueWhite": "#faebd7", "AntiqueWhite1": "#ffefdb", "AntiqueWhite2": "#eedfcc",
+COLOURS = {"AliceBlue": "#f0f8ff", "AntiqueWhite": "#faebd7", "AntiqueWhite1": "#ffefdb", "AntiqueWhite2": "#eedfcc",
           "AntiqueWhite3": "#cdc0b0", "AntiqueWhite4": "#8b8378", "aquamarine1": "#7fffd4", "aquamarine2": "#76eec6",
           "aquamarine4": "#458b7", "azure1": "#f0ffff"}
 
@@ -6,8 +6,8 @@ valid = False
 
 colour_name = input("enter a colour name: ")
 while not valid:
-    if colour_name in COLOUR:
-        print("Your colour code is", COLOUR[colour_name])
+    if colour_name in COLOURS:
+        print("Your colour code is", COLOURS[colour_name])
         valid = True
     else:
         print("That name is not in our dictionary")
