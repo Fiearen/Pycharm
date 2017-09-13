5475ee50da99bb7d0c7e8a93f8a3972f9f94c237
jc463548 <Jackson.Finnigan@my.jcu.edu.au>
2017-09-13 13:50:43 +1000

    Edit

--- prac_05/colour_names.py
+++ prac_05/colour_names.py
@@ -1,14 +1,10 @@
 COLOURS = {"AliceBlue": "#f0f8ff", "AntiqueWhite": "#faebd7", "AntiqueWhite1": "#ffefdb", "AntiqueWhite2": "#eedfcc",
-          "AntiqueWhite3": "#cdc0b0", "AntiqueWhite4": "#8b8378", "aquamarine1": "#7fffd4", "aquamarine2": "#76eec6",
-          "aquamarine4": "#458b7", "azure1": "#f0ffff"}
-
-valid = False
+           "AntiqueWhite3": "#cdc0b0", "AntiqueWhite4": "#8b8378", "aquamarine1": "#7fffd4", "aquamarine2": "#76eec6",
+           "aquamarine4": "#458b7", "azure1": "#f0ffff"}
 
 colour_name = input("enter a colour name: ")
-while not valid:
-    if colour_name in COLOURS:
-        print("Your colour code is", COLOURS[colour_name])
-        valid = True
-    else:
-        print("That name is not in our dictionary")
-        colour_name = input("enter a colour name: ")
+while colour_name not in COLOURS:
+    print("That name is not in our dictionary")
+    colour_name = input("enter a colour name: ")
+
+print("Your colour code is", COLOURS[colour_name])
