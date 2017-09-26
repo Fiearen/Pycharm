b439b80be94b67ecf81595dc1d8060cceb995812
Jackson Finnigan <31056058+JacksonFinnigan@users.noreply.github.com>
2017-09-26 17:58:12 +1000

    Finished Practical 07

--- prac_07/guitar_attributes.py
+++ prac_07/guitar_attributes.py
@@ -4,16 +4,16 @@ Programming 1 Practical 7
 CURRENT_YEAR = 2017
 
 class Guitar:
-    def __init__(self, name="", year=0, cost="$0"):
+    def __init__(self, name="", year=0, cost=0):
         self.name = name
         self.year = year
         self.cost = cost
 
-    def __str__(self):
+    def __str__(self, guitar_name_len=0):
         return "{} ({}) : {}".format(self.name, self.year, self.cost)
 
     def is_vintage(self):
-        if 2017 - self.year >= 50:
+        if 2017 - self.year <= 50:
             return True
         return False
 
--- prac_07/guitars.py
+++ prac_07/guitars.py
@@ -3,22 +3,31 @@ Programming 1 Practical 7
 """
 
 from prac_07.guitar_attributes import Guitar
+
+
 def main():
-    my_guitars = [Guitar("Gibson L-5 CES", 1922,  "$16,035.40"), Guitar("Another Guitar", 2012,  "$1000")]
+    i = 0
+    my_guitars = [Guitar("Gibson L-5 CES", 1922, 16035.40), Guitar("Another Guitar", 2012, 1000)]
     while True:
         new_guitar_name = input("Name: ")
         if new_guitar_name == "":
             break
-        new_guitar_year = input("Year: ")
+        new_guitar_year = int(input("Year: "))
         if new_guitar_year == "":
             break
-        new_guitar_price = input("Price: ")
+        new_guitar_price = int(input("Price: "))
         if new_guitar_price == "":
             break
         print(Guitar(new_guitar_name, new_guitar_year, new_guitar_price), "added.")
         my_guitars.append(Guitar(new_guitar_name, new_guitar_year, new_guitar_price))
     for guitar in my_guitars:
-        print("These are my Guitars:", my_guitars)
+        if guitar.is_vintage():
+            vintage_string = ""
+        else:
+            vintage_string = "vintage"
+        print("Guitar {}: {:>20} ({}), worth ${:10,.2f} {}".format(i + 1, guitar.name, guitar.year, guitar.cost,
+                                                                   vintage_string))
+        i += 1
 
 
 main()
